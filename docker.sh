c3b85e01a5ae82391bb44487304e4296e927fecf
Nitin Sati <nitinsattidelhi@gmail.com>
2019-06-20 18:23:51 +0530

    This is the file we need to run on linux system using command './docker.sh' and it will pull customized docker image present at docker hub, run a container named 'API_CONTAINER' on linux machine and put the user inside the container. After the successful execution of this script, we will be insidedocker and we can run 'python /var/tmp/python-script.py' to get rotten tomatoes detail of any movie. User will be asked for movie during script execution.

--- docker.sh
+++ docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script will pull image from docker hub and copy a python source code file present in current directory inside the docker hub
+
 docker pull nitinsatti/centos-repo:API
 docker run --name API_CONTAINER -td docker.io/nitinsatti/centos-repo:API
 docker cp python-script.py API_CONTAINER:/var/tmp/python-script.py
