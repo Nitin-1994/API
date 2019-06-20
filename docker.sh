3aa1998e1ca7f13582fb0b52dab89f63fac9d6dd
Nitin Sati <nitinsattidelhi@gmail.com>
2019-06-20 05:33:21 +0000

    Run this file on Linux system

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker pull nitinsatti/centos-repo:API
 docker run --name API_CONTAINER -td docker.io/nitinsatti/centos-repo:API
-docker cp second.py API_CONTAINER:/var/tmp/python-script.py
+docker cp python-script.py API_CONTAINER:/var/tmp/python-script.py
 docker exec -it API_CONTAINER bash 
