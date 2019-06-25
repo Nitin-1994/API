97a0db0c104d11252b75d3d8e117d0e2f369d065
Nitin Sati <nitinsatti1994@gmail.com>
2019-06-25 09:58:35 -0700

    This file will help us to get rotten tomatoes value of a movie and it'll store inside container at location /var/tmp/ and will be stored as Rotten_Tomatoes.py, you can run command 'python /var/tmp/Rotten_Tomatoes.py' to run it

--- Rotten_Tomatoes.py
+++ Rotten_Tomatoes.py
@@ -15,27 +15,31 @@ def main():
     data = r.json()
     try:
         rating = data['Ratings']
-
-        for i in range(len(rating)):
-            if rating[i]['Source'] == 'Rotten Tomatoes':
-                rotten_value = rating[i]['Value']
-
-        print(f"Movie Name : {movie_name} \nRotten Tomatoes : {rotten_value}")
-
-        if rotten_value:
-            rotten_value=int(rotten_value[:-1])
-            if rotten_value > 30:
-                print("Movies has high rating of Rotten Tomatoes, don't watch")
-            else:
-                print("Movies has low rating of Rotten Tomatoes, you can watch")
-        else:
-            print("Rotten tomatoes value is not present")
-
     except:
         print("The is no such movie, Do you want to try again ?")
         output=input("Enter y/n :")
         if output=="y":
             goto .begin
+        else:
+           goto .end
+
+    for i in range(len(rating)):
+        if rating[i]['Source'] == 'Rotten Tomatoes':
+            rotten_value = rating[i]['Value']
+
+    print(f"Movie Name : {movie_name} \nRotten Tomatoes : {rotten_value}")
+
+    if rotten_value:
+        rotten_value=int(rotten_value[:-1])
+        if rotten_value > 30:
+            print("Movies has high rating of Rotten Tomatoes, don't watch")
+        else:
+            print("Movies has low rating of Rotten Tomatoes, you can watch")
+    else:
+        print("Rotten tomatoes value is not present")
+
+    label .end
 
 main()
+
 
