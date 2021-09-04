5376f70d5f8a37b3f08f0f33d241527b7662a785
Nitin Sati <nitinsatti1994@gmail.com>
2021-09-04 05:11:05 -0400

    doing

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script will pull image from docker hub and copy a python source code file present in current directory inside the docker hub
+#w This script will pull image from docker hub and copy a python source code file present in current directory inside the docker hub
 docker pull nitinsatti/centos-repo:Rotten_tomatoes_Container
 docker run --name Rotten_Tomatoes_Container -td docker.io/nitinsatti/centos-repo:Rotten_tomatoes_Container
 docker cp Rotten_Tomatoes.py Rotten_Tomatoes_Container:/var/tmp/Rotten_Tomatoes.py
